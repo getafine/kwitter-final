bdf477b172b0b1f052ed04e4e9e8d8dd33600dde
getafine <132389051+getafine@users.noreply.github.com>
2023-11-07 17:58:33 -0300

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -66,5 +66,5 @@ function updateLike(message_id){
 function logout(){
     localStorage.removeItem("user_name");
     localStorage.removeItem("room_name");
-    window.location.replace("kwitter.html");
+    window.location.replace("index.html");
 }
